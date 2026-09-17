493f6147e65793e28d7951f59449a8aa5cdf0739
Sanjeevan23 <yogaratnamsanjeevan@gmail.com>
2026-09-17 23:06:06 +0530

    px added

--- src/components/more/PrivacySection.tsx
+++ src/components/more/PrivacySection.tsx
@@ -619,7 +619,7 @@ function PolicySectionBlock({ title, blocks }: PolicySection) {
 
 export default function PrivacyPolicy() {
     return (
-        <div className="font-poppins mx-auto flex max-w-[910px] flex-col bg-white">
+        <div className="font-poppins mx-auto px-4 flex max-w-[910px] flex-col bg-white">
             <div className="flex flex-col items-center text-center">
                 <div className="text-[16px] sm:text-[17px] md:text-[18px] font-medium leading-[150%] text-dark">
                     INAI.LK
